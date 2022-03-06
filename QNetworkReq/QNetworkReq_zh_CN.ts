90b506005cdd1c45f83f19b4b10e25ef52367843
Feafly <409435808@qq.com>
2022-03-06 21:22:22 +0800

    Add project QNetworkReq by QtCreator

---
+++ QNetworkReq/QNetworkReq_zh_CN.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>Widget</name>
+    <message>
+        <location filename="widget.ui" line="14"/>
+        <source>Widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widget.ui" line="25"/>
+        <source>开始请求</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
