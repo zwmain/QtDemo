87cb520325f140253674ed9e483fade90bcbe740
Feafly <409435808@qq.com>
2022-03-07 22:16:43 +0800

    Add cmake qt prefix

--- QNetworkReq/QNetworkReq_zh_CN.ts
+++ QNetworkReq/QNetworkReq_zh_CN.ts
@@ -5,11 +5,13 @@
     <name>Widget</name>
     <message>
         <location filename="widget.ui" line="14"/>
+        <location filename="build/QNetworkReq_autogen/include/ui_widget.h" line="74"/>
         <source>Widget</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="widget.ui" line="25"/>
+        <location filename="build/QNetworkReq_autogen/include/ui_widget.h" line="75"/>
         <source>开始请求</source>
         <translation type="unfinished"></translation>
     </message>
