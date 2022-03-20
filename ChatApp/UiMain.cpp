6533580763693a85991f9c25b8d518f34496aae1
Feafly <409435808@qq.com>
2022-03-20 21:00:01 +0800

    Add ui for config host and port

--- ChatApp/UiMain.cpp
+++ ChatApp/UiMain.cpp
@@ -2,8 +2,32 @@
 
 UiMain::UiMain(QWidget *parent) : QWidget(parent)
 {
+    this->resize(800, 600);
+
     _ui_lay_main = new QVBoxLayout(this);
     _ui_lay_main->setContentsMargins(1, 1, 1, 1);
+
+    _ui_wid_cfg = new QWidget(this);
+    _ui_wid_cfg->setSizePolicy(
+        QSizePolicy(QSizePolicy::Preferred, QSizePolicy::Maximum));
+    _ui_lay_cfg = new QGridLayout(_ui_wid_cfg);
+    _ui_lay_cfg->setContentsMargins(0, 0, 0, 0);
+    _ui_lay_main->addWidget(_ui_wid_cfg);
+
+    _ui_lab_host = new QLabel("地址：", _ui_wid_cfg);
+    _ui_lay_cfg->addWidget(_ui_lab_host, 0, 0, 1, 1);
+
+    _ui_lin_host = new QLineEdit(_ui_wid_cfg);
+    _ui_lay_cfg->addWidget(_ui_lin_host, 0, 1, 1, 1);
+
+    _ui_lab_port = new QLabel("端口：", _ui_wid_cfg);
+    _ui_lay_cfg->addWidget(_ui_lab_port, 0, 2, 1, 1);
+
+    _ui_lin_port = new QLineEdit(_ui_wid_cfg);
+    _ui_lay_cfg->addWidget(_ui_lin_port, 0, 3, 1, 1);
+
+    _ui_btn_cfg = new QPushButton("确认", _ui_wid_cfg);
+    _ui_lay_cfg->addWidget(_ui_btn_cfg, 0, 4, 1, 1);
 
     _ui_sp_main = new QSplitter(Qt::Orientation::Horizontal, this);
     _ui_lay_main->addWidget(_ui_sp_main);
@@ -19,13 +43,41 @@ UiMain::UiMain(QWidget *parent) : QWidget(parent)
     _ui_lay_input = new QVBoxLayout(_ui_wid_input);
     _ui_lay_input->setContentsMargins(0, 0, 0, 0);
     _ui_lay_input->setSpacing(1);
-    
+
     _ui_txt_input = new QTextEdit(_ui_wid_input);
     _ui_btn_send = new QPushButton("发送", _ui_wid_input);
     _ui_lay_input->addWidget(_ui_txt_input);
     _ui_lay_input->addWidget(_ui_btn_send);
+
+    _server = new QTcpServer(this);
+    bool is_success = false;
+    quint16 port = 8848;
+    while (!is_success && port < 10000)
+    {
+        is_success = _server->listen(QHostAddress("127.0.0.1"), port);
+        ++port;
+    }
+
+    connect(_server, &QTcpServer::acceptError, this, &UiMain::onAcceptError);
+    connect(_server, &QTcpServer::newConnection, this, &UiMain::onNewConnection);
+
+    _sock = new QTcpSocket(this);
 }
 
 UiMain::~UiMain()
 {
+    if (_server && _server->isListening())
+    {
+        _server->close();
+    }
+}
+
+void UiMain::onAcceptError(QAbstractSocket::SocketError socketError)
+{
+    // todo
+}
+
+void UiMain::onNewConnection()
+{
+    // todo
 }
--- ChatApp/UiMain.h
+++ ChatApp/UiMain.h
@@ -3,7 +3,12 @@
 #include <QTextEdit>
 #include <QPushButton>
 #include <QVBoxLayout>
+#include <QGridLayout>
 #include <QSplitter>
+#include <QLabel>
+#include <QLineEdit>
+#include <QtNetwork/QTcpServer>
+#include <QtNetwork/QTcpSocket>
 
 class UiMain : public QWidget
 {
@@ -14,6 +19,13 @@ public:
 private:
     // UI
     QVBoxLayout *_ui_lay_main = nullptr;
+    QWidget *_ui_wid_cfg = nullptr;
+    QGridLayout *_ui_lay_cfg = nullptr;
+    QLabel *_ui_lab_host = nullptr;
+    QLabel *_ui_lab_port = nullptr;
+    QLineEdit *_ui_lin_host = nullptr;
+    QLineEdit *_ui_lin_port = nullptr;
+    QPushButton *_ui_btn_cfg = nullptr;
     QSplitter *_ui_sp_main = nullptr;
     QListWidget *_ui_list_usr = nullptr;
     QSplitter *_ui_sp_txt = nullptr;
@@ -22,4 +34,12 @@ private:
     QVBoxLayout *_ui_lay_input = nullptr;
     QTextEdit *_ui_txt_display = nullptr;
     QPushButton *_ui_btn_send = nullptr;
+
+    // TCP
+    QTcpServer *_server = nullptr;
+    QTcpSocket *_sock = nullptr;
+
+    // Event function
+    void onAcceptError(QAbstractSocket::SocketError socketError);
+    void onNewConnection();
 };
