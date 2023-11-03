860c59182e25c19cc7779425cc25efd6853a7322
Feafly <409435808@qq.com>
2023-11-03 11:41:16 +0800

    Add translation from cmake

---
+++ Demo5.15/Demo_en.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>UiMainWindow</name>
+    <message>
+        <location filename="MainWindow.ui" line="14"/>
+        <location filename="build/Demo_autogen/include_Debug/ui_MainWindow.h" line="71"/>
+        <source>主页</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="32"/>
+        <location filename="build/Demo_autogen/include_Debug/ui_MainWindow.h" line="72"/>
+        <source>搜索</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
