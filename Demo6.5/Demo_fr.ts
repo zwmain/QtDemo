7ff3ee4d560b083d5b7e666436dcf2966d42ee2a
zwmain <zwmain@outlook.com>
2023-11-04 15:37:25 +0800

    Update ts file

--- Demo6.5/Demo_fr.ts
+++ Demo6.5/Demo_fr.ts
@@ -19,11 +19,13 @@
     <name>uiMainWid</name>
     <message>
         <location filename="MainWindow.ui" line="14"/>
+        <location filename="build/Demo_autogen/include/ui_MainWindow.h" line="71"/>
         <source>主页</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="32"/>
+        <location filename="build/Demo_autogen/include/ui_MainWindow.h" line="72"/>
         <source>搜索</source>
         <translation type="unfinished"></translation>
     </message>
